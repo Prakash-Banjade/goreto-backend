a329024da2e0e29126aa6ea10047023fc4d6648d
Prakash Banjade <prakashbanjade191@gmail.com>
2024-06-18 19:11:22 +0545

    udpate swagger info, add modules

--- src/config/swagger.config.ts
+++ src/config/swagger.config.ts
@@ -1,12 +1,17 @@
 import { INestApplication } from "@nestjs/common";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { AuthModule } from "src/auth/auth.module";
+import { CategoriesModule } from "src/categories/categories.module";
+import { CutTypesModule } from "src/product-filters/cut-types/cut-types.module";
+import { PreparationsModule } from "src/product-filters/preparations/preparations.module";
+import { ProductsModule } from "src/products/products.module";
+import { ShippingAddressesModule } from "src/shipping-addresses/shipping-addresses.module";
 import { UsersModule } from "src/users/users.module";
 
 export function setupSwagger(app: INestApplication): void {
     const config = new DocumentBuilder()
-        .setTitle('My API Title')
-        .setDescription('Backend API documentation for NestJs application')
+        .setTitle('Goreto - A meat shop')
+        .setDescription('Backend API documentation for Goreto.')
         .setVersion('1.0')
         .addBearerAuth(
             {
@@ -21,20 +26,20 @@ export function setupSwagger(app: INestApplication): void {
         .build();
 
     const document = SwaggerModule.createDocument(app, config, {
-        include: [AuthModule, UsersModule],
+        include: [AuthModule, UsersModule, ProductsModule, CategoriesModule, CutTypesModule, PreparationsModule, ShippingAddressesModule],
     });
 
     SwaggerModule.setup('api', app, document, {
-        customSiteTitle: 'My API Title',
-        // customfavIcon: 'https://avatars.githubusercontent.com/u/6936373?s=200&v=4',
-        // customJs: [
-        //     'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
-        //     'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js',
-        // ],
-        // customCssUrl: [
-        //     'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
-        //     'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css',
-        //     'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
-        // ],
+        customSiteTitle: 'Goreto - A meat shop',
+        customfavIcon: 'https://avatars.githubusercontent.com/u/6936373?s=200&v=4',
+        customJs: [
+            'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
+            'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js',
+        ],
+        customCssUrl: [
+            'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+            'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css',
+            'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
+        ],
     });
 }
