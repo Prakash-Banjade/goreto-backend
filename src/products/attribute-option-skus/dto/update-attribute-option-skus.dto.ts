2e8b6685b62eddb054df19b2d9e451296fcf5e52
Prakash Banjade <prakashbanjade191@gmail.com>
2024-07-03 15:21:16 +0545

    remove unused files

--- src/products/attribute-option-skus/dto/update-attribute-option-skus.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateAttributeOptionSkusDto } from './create-attribute-option-skus.dto';
-
-export class UpdateAttributeOptionSkusDto extends PartialType(CreateAttributeOptionSkusDto) {}
