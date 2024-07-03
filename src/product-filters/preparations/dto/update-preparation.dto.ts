cfa9ac27fa465bb8211b6c779e4051c31acc4d8a
Prakash Banjade <prakashbanjade191@gmail.com>
2024-07-03 14:18:14 +0545

    remove preparation, cut type, discount

--- src/product-filters/preparations/dto/update-preparation.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreatePreparationDto } from './create-preparation.dto';
-
-export class UpdatePreparationDto extends PartialType(CreatePreparationDto) {}
